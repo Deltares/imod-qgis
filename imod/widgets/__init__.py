d59e44d09947a0381c1050d851b362045489d9d3
JoerivanEngelen <joerivanengelen@hotmail.com>
2021-04-01 16:17:39 +0200

    removed newline

--- imod/widgets/__init__.py
+++ imod/widgets/__init__.py
@@ -1,5 +1,4 @@
 from .pseudocolor_widget import ImodPseudoColorWidget
 from .unique_color_widget import ImodUniqueColorWidget
 from .dataset_variable_widget import VariablesWidget, MultipleVariablesWidget
-
 from .maptools import RectangleMapTool, LineGeometryPickerWidget
