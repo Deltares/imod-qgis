935977774cb532bba94dfa0c512101fd8f969260
Huite Bootsma <huitebootsma@gmail.com>
2021-04-11 00:40:59 +0200

    Update legend, link plot and vertex on map

--- imod/cross_section/cross_section_widget.py
+++ imod/cross_section/cross_section_widget.py
@@ -16,6 +16,7 @@ from PyQt5.QtWidgets import (
     QSizePolicy,
     QSplitter,
     QFrame,
+    QMainWindow,
 )
 from PyQt5.QtGui import QColor, QDropEvent
 from PyQt5.QtCore import Qt, pyqtSignal
@@ -23,6 +24,7 @@ from qgis.gui import (
     QgsMapLayerComboBox,
     QgsRubberBand,
     QgsColorRampButton,
+    QgsVertexMarker,
 )
 from qgis.core import (
     QgsGeometry,
@@ -43,12 +45,8 @@ import pyqtgraph as pg
 from pyqtgraph.GraphicsScene.exportDialog import ExportDialog
 from pyqtgraph.graphicsItems.GraphicsWidget import GraphicsWidget
 
-from .data import MeshData, MeshLineData, RasterLineData, BoreholeData
-from ..widgets import (
-    MultipleVariablesWidget, 
-    VariablesWidget, 
-    LineGeometryPickerWidget
-)
+from .cross_section_data import MeshData, MeshLineData, RasterLineData, BoreholeData
+from ..widgets import MultipleVariablesWidget, VariablesWidget, LineGeometryPickerWidget
 from ..utils.layers import groupby_variable, get_group_names
 from ..ipf import IpfType, read_associated_borehole
 
@@ -62,7 +60,7 @@ class UpdatingQgsMapLayerComboBox(QgsMapLayerComboBox):
     def enterEvent(self, e):
         self.update_layers()
         super(UpdatingQgsMapLayerComboBox, self).enterEvent(e)
-    
+
     def update_layers(self):
         # Allow:
         # * Point data with associated IPF borehole data
@@ -71,9 +69,9 @@ class UpdatingQgsMapLayerComboBox(QgsMapLayerComboBox):
         excepted_layers = []
         for layer in QgsProject.instance().mapLayers().values():
             if not (
-                (layer.type() == QgsMapLayerType.MeshLayer) or
-                (layer.type() == QgsMapLayerType.RasterLayer) or
-                (layer.customProperty("ipf_type") == IpfType.BOREHOLE.name)
+                (layer.type() == QgsMapLayerType.MeshLayer)
+                or (layer.type() == QgsMapLayerType.RasterLayer)
+                or (layer.customProperty("ipf_type") == IpfType.BOREHOLE.name)
             ):
                 excepted_layers.append(layer)
         self.setExceptedLayerList(excepted_layers)
@@ -86,7 +84,9 @@ class ColorRampView(QgsColorRampButton):
 
     A new clicked signal is introduced instead, which is used to open a dialog.
     """
+
     clicked = pyqtSignal()
+
     def mousePressEvent(self, _):
         self.clicked.emit()
 
@@ -100,12 +100,13 @@ class StyleTree(QTreeWidget):
     This class overloads the DropEvent, ensuring the removed item is copied in
     full, including widgets.
 
-    See also:     
+    See also:
     https://www.qtcentre.org/threads/40500-QTreeWidget-setItemWidget()-item-disappears-after-moving-item
 
     This class also automatically configures the desired drag and drop behavior,
     so items can be ordered (only) at the top level.
     """
+
     def __init__(self, parent=None):
         super(StyleTree, self).__init__(parent)
         self.setDragDropMode(QAbstractItemView.InternalMove)
@@ -124,6 +125,12 @@ class StyleTree(QTreeWidget):
             self.insertTopLevelItem(destination, new)
         new.set_widgets()
 
+    def remove(self):
+        item = self.currentItem()
+        if item is not None:
+            self.takeTopLevelItem(self.indexOfTopLevelItem(item))
+
+
 class StyleTreeItem(QTreeWidgetItem):
     """
     This holds the items of the styling tree:
@@ -134,6 +141,7 @@ class StyleTreeItem(QTreeWidgetItem):
     * A legend checkbox whether its legend is displayed
     * A view on the ColorRamp
     """
+
     def __init__(self, name, layer_type, section_data):
         super(StyleTreeItem, self).__init__()
         self.show_checkbox = QCheckBox()
@@ -145,12 +153,12 @@ class StyleTreeItem(QTreeWidgetItem):
         self.legend_checkbox.setChecked(True)
         self.colors_view.setEnabled(False)
         # Do not allow inserting items into these child items
-        self.setFlags(self.flags() ^Qt.ItemIsDropEnabled)
+        self.setFlags(self.flags() ^ Qt.ItemIsDropEnabled)
         self.section_data = section_data
         self.update_ramp()
         self.section_data.colors_changed.connect(self.update_ramp)
         self.colors_view.clicked.connect(self.section_data.edit_colors)
-    
+
     def set_widgets(self):
         """
         A QTreeWidget maintains ownership of the widgets, rather than the item
@@ -179,7 +187,7 @@ class StyleTreeItem(QTreeWidgetItem):
         new.legend_checkbox.setChecked(item.legend_checkbox.isChecked())
         new.colors_view.setEnabled(item.colors_view.isEnabled())
         return new
-        
+
 
 class ImodCrossSectionWidget(QWidget):
     # TODO: Include time selection box
@@ -192,15 +200,19 @@ class ImodCrossSectionWidget(QWidget):
         self.layer_selection.setMinimumWidth(200)
 
         self.variable_selection = VariablesWidget()
-        self.variable_selection.dataset_variable_changed.connect(self.set_variable_layernumbers)
+        self.variable_selection.dataset_variable_changed.connect(
+            self.set_variable_layernumbers
+        )
         self.multi_variable_selection = MultipleVariablesWidget()
-        
+
         self.add_button = QPushButton("Add")
         self.add_button.clicked.connect(self.add)
         self.add_button.setMaximumWidth(70)
 
         self.line_picker = LineGeometryPickerWidget(iface)
         self.line_picker.geometries_changed.connect(self.on_geometries_changed)
+        self.line_picker.button.clicked.connect(self.clear_plot)
+        self.line_picker.button.clicked.connect(self.hide_vertex)
 
         self.as_line_checkbox = QCheckBox("As line(s)")
         self.as_line_checkbox.setMaximumWidth(90)
@@ -208,10 +220,13 @@ class ImodCrossSectionWidget(QWidget):
         self.plot_button = QPushButton("Plot")
         self.plot_button.clicked.connect(self.plot)
 
-        self.clear_button = QPushButton("Clear")
-        self.clear_button.clicked.connect(self.clear_plot)
+        self.detach_button = QPushButton("Detach")
+        self.detach_button.clicked.connect(self.detach)
 
         self.plot_widget = pg.PlotWidget()
+        self.mouse_proxy = pg.SignalProxy(
+            self.plot_widget.scene().sigMouseMoved, rateLimit=25, slot=self.track_mouse
+        )
         self.plot_widget.showGrid(x=True, y=True)
         self.plot_widget.addLegend()
         self.legend = self.plot_widget.getPlotItem().legend
@@ -225,28 +240,34 @@ class ImodCrossSectionWidget(QWidget):
         self.dynamic_resolution_box.setChecked(True)
 
         self.resolution_spinbox = QDoubleSpinBox()
-        self.resolution_spinbox.setRange(0.01, 10000.)
+        self.resolution_spinbox.setRange(0.01, 10000.0)
         self.resolution_spinbox.setValue(50.0)
 
         self.buffer_label = QLabel("Search buffer")
         self.buffer_spinbox = QDoubleSpinBox()
-        self.buffer_spinbox.setRange(0., 10000.)
+        self.buffer_spinbox.setRange(0.0, 10000.0)
         self.buffer_spinbox.setValue(250.0)
         self.buffer_spinbox.valueChanged.connect(self.refresh_buffer)
 
-#        self.style_tree = QTreeWidget()
         self.style_tree = StyleTree()
         self.style_tree.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Preferred)
         self.style_tree.setColumnCount(5)
-        self.style_tree.setHeaderLabels(["", "name", "type", "legend", "colors"])
+        self.style_tree.setHeaderLabels(["", "name", "type", "legend", "symbology"])
+
+        self.remove_button = QPushButton("Remove")
+        self.remove_button.clicked.connect(self.remove)
 
         self.style_tree.setDragDropMode(QAbstractItemView.InternalMove)
         self.style_tree.setDragEnabled(True)
-        
+
         # Selection geometry
         self.rubber_band = None
         self.show_buffer = True
         self.buffer_rubber_band = None
+        self.point_rubber_band = QgsVertexMarker(self.iface.mapCanvas())
+        self.point_rubber_band.setColor(RUBBER_BAND_COLOR)
+        self.point_rubber_band.setIconSize(5)
+        self.point_rubber_band.setPenWidth(3)
 
         # Setup layout
         first_row = QHBoxLayout()
@@ -257,8 +278,8 @@ class ImodCrossSectionWidget(QWidget):
         first_row.addWidget(self.buffer_spinbox)
         first_row.addWidget(self.plot_button)
         first_row.addWidget(self.export_button)
-        first_row.addWidget(self.clear_button)
         first_row.addStretch()
+        first_row.addWidget(self.detach_button)
 
         selection_row = QHBoxLayout()
         selection_row.addWidget(self.layer_selection)
@@ -271,6 +292,10 @@ class ImodCrossSectionWidget(QWidget):
         layer_column = QVBoxLayout(layer_column_frame)
         layer_column.addLayout(selection_row)
         layer_column.addWidget(self.style_tree)
+        bottom_row = QHBoxLayout()
+        bottom_row.addWidget(self.remove_button)
+        bottom_row.addStretch()
+        layer_column.addLayout(bottom_row)
 
         plot_column = QHBoxLayout()
         plot_column.addWidget(self.plot_widget)
@@ -286,7 +311,7 @@ class ImodCrossSectionWidget(QWidget):
         layout.addLayout(first_row)
         layout.addWidget(second_row)
         self.setLayout(layout)
-        
+
         # Run a single time initialize the combo boxes
         self.layer_selection.update_layers()
         self.on_layer_changed()
@@ -308,7 +333,9 @@ class ImodCrossSectionWidget(QWidget):
             )
             self.buffer_rubber_band.setColor(BUFFER_RUBBER_BAND_COLOR)
             buffer_distance = self.buffer_spinbox.value()
-            self.buffer_rubber_band.setToGeometry(self.line_picker.geometries[0].buffer(buffer_distance, 4), None)
+            self.buffer_rubber_band.setToGeometry(
+                self.line_picker.geometries[0].buffer(buffer_distance, 4), None
+            )
 
     def add(self):
         layer = self.layer_selection.currentLayer()
@@ -321,10 +348,10 @@ class ImodCrossSectionWidget(QWidget):
             variable = self.variable_selection.dataset_variable
             layers = self.multi_variable_selection.checked_variables()
             if self.as_line_checkbox.isChecked():
-                data = MeshLineData(layer, self.variables_indexes, variable, layers) 
+                data = MeshLineData(layer, self.variables_indexes, variable, layers)
                 layer_item = StyleTreeItem(f"{name}: {variable}", "mesh: lines", data)
             else:
-                data = MeshData(layer, self.variables_indexes, variable, layers) 
+                data = MeshData(layer, self.variables_indexes, variable, layers)
                 layer_item = StyleTreeItem(f"{name}: {variable}", "mesh", data)
         elif layer_type == QgsMapLayerType.RasterLayer:
             variables = self.multi_variable_selection.checked_variables()
@@ -340,16 +367,22 @@ class ImodCrossSectionWidget(QWidget):
             )
         self.style_tree.addTopLevelItem(layer_item)
         layer_item.set_widgets()
+        layer_item.show_checkbox.stateChanged.connect(self.plot)
         data.colors_changed.connect(self.plot)
+        layer_item.legend_checkbox.stateChanged.connect(self.update_legend)
+
+    def remove(self):
+        self.style_tree.remove()
 
     def plot(self):
         if len(self.line_picker.geometries) == 0:
             return
+        self.clear_plot()
         nrow = self.style_tree.topLevelItemCount()
         load_kwargs = {
             "geometry": self.line_picker.geometries[0],
             "resolution": self.resolution_spinbox.value(),
-            "buffer": self.buffer_spinbox.value(),
+            "buffer_distance": self.buffer_spinbox.value(),
         }
         for i in range(nrow):
             item = self.style_tree.topLevelItem(i)
@@ -360,25 +393,40 @@ class ImodCrossSectionWidget(QWidget):
                     data.load(**load_kwargs)
                     item.colors_view.setEnabled(True)
                 data.plot(self.plot_widget)
+        self.update_legend()
+
+    def update_legend(self):
+        self.legend.clear()
+        nrow = self.style_tree.topLevelItemCount()
+        for i in range(nrow):
+            item = self.style_tree.topLevelItem(i)
+            if item.legend_checkbox.isChecked():
+                data = item.section_data
+                data.add_to_legend(self.legend)
 
     def set_variable_names(self):
         layer = self.layer_selection.currentLayer()
         if layer.type() == QgsMapLayerType.MeshLayer:
             indexes, names = get_group_names(layer)
             self.variables_indexes = groupby_variable(names, indexes)
-            self.variable_selection.menu_datasets.populate_actions(self.variables_indexes.keys())
+            self.variable_selection.menu_datasets.populate_actions(
+                self.variables_indexes.keys()
+            )
+            self.variable_selection.menu_datasets.check_first()
             self.set_variable_layernumbers()
         elif layer.type() == QgsMapLayerType.RasterLayer:
-            variables = [] 
+            variables = []
             self.variables_indexes = {}
             for i in range(1, layer.bandCount() + 1):
                 name = layer.bandName(i)
                 variables.append(name)
                 self.variables_indexes[name] = i
             self.multi_variable_selection.menu_datasets.populate_actions(variables)
+            self.multi_variable_selection.menu_datasets.check_first()
         elif layer.customProperty("ipf_type") == IpfType.BOREHOLE.name:
             variables = layer.customProperty("ipf_assoc_columns").split("␞")
             self.variable_selection.set_layer(variables)
+            self.variable_selection.menu_datasets.check_first()
 
     def set_variable_layernumbers(self):
         layer = self.layer_selection.currentLayer()
@@ -409,6 +457,11 @@ class ImodCrossSectionWidget(QWidget):
             resolution = geometry.length() / 300.0
             self.resolution_spinbox.setValue(resolution)
 
+        nrow = self.style_tree.topLevelItemCount()
+        for i in range(nrow):
+            item = self.style_tree.topLevelItem(i)
+            item.section_data.clear()
+
     def on_layer_changed(self):
         layer = self.layer_selection.currentLayer()
         if layer is None:
@@ -433,6 +486,27 @@ class ImodCrossSectionWidget(QWidget):
         self.set_variable_names()
         self.refresh_buffer()
 
+    def track_mouse(self, event):
+        if len(self.line_picker.geometries) == 0:
+            self.hide_vertex()
+            return
+        geometry = self.line_picker.geometries[0]
+        point_geometry = geometry.interpolate(
+            self.plot_widget.plotItem.vb.mapSceneToView(event[0]).x()
+        )
+        if point_geometry:  # could also be null geometry
+            self.point_rubber_band.setCenter(point_geometry.asPoint())
+            self.point_rubber_band.show()
+
     def export(self):
         plot_item = self.plot_widget.plotItem
         self.export_dialog.show(plot_item)
+
+    def detach(self):
+        new = QMainWindow()
+        new.setWindowTitle("iMOD Cross Section plot")
+        self.setParent(new)
+        new.show()
+
+    def hide_vertex(self):
+        self.point_rubber_band.hide()
