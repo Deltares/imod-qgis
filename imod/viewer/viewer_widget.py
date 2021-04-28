970c2476ae261c51f1a8651a7d001ebc77aa1978
JoerivanEngelen <joerivanengelen@hotmail.com>
2021-04-28 15:56:59 +0200

    ran black

--- imod/viewer/viewer_widget.py
+++ imod/viewer/viewer_widget.py
@@ -9,8 +9,8 @@ from PyQt5.QtWidgets import (
 from qgis.gui import QgsExtentGroupBox, QgsMapLayerComboBox
 from qgis.core import (
     QgsProject,
-    QgsMapLayerProxyModel, 
-    QgsMeshDatasetIndex, 
+    QgsMapLayerProxyModel,
+    QgsMeshDatasetIndex,
     QgsMapLayerType,
 )
 
@@ -24,11 +24,12 @@ from ..ipf import IpfType
 
 import uuid
 
+
 class UpdatingQgsMapLayerComboBox(QgsMapLayerComboBox):
     def enterEvent(self, e):
         self.update_layers()
         super(UpdatingQgsMapLayerComboBox, self).enterEvent(e)
-    
+
     def update_layers(self):
         # Allow:
         # * Point data with associated IPF borehole data
@@ -36,8 +37,8 @@ class UpdatingQgsMapLayerComboBox(QgsMapLayerComboBox):
         excepted_layers = []
         for layer in QgsProject.instance().mapLayers().values():
             if not (
-                (layer.type() == QgsMapLayerType.MeshLayer) or
-                (layer.customProperty("ipf_type") == IpfType.BOREHOLE.name)
+                (layer.type() == QgsMapLayerType.MeshLayer)
+                or (layer.customProperty("ipf_type") == IpfType.BOREHOLE.name)
             ):
                 excepted_layers.append(layer)
         self.setExceptedLayerList(excepted_layers)
@@ -51,24 +52,24 @@ class ImodViewerWidget(QWidget):
         self.crs = self.canvas.mapSettings().destinationCrs()
         self.server = Server()
 
-        #Layer selection
+        # Layer selection
         self.layer_selection = UpdatingQgsMapLayerComboBox()
 
-        #Draw fence diagram button
+        # Draw fence diagram button
         self.line_picker = MultipleLineGeometryPickerWidget(canvas)
 
-        #Draw extent button      
-        #TODO: Create seperate widget for extent_button, so that the right click button can be used to unset geometry
+        # Draw extent button
+        # TODO: Create seperate widget for extent_button, so that the right click button can be used to unset geometry
         self.extent_button = QPushButton("Draw extent")
         self.extent_button.clicked.connect(self.draw_extent)
         self.rectangle_tool = RectangleMapTool(self.canvas)
         self.rectangle_tool.rectangleCreated.connect(self.set_bbox)
 
-        #Extent box
+        # Extent box
         self.extent_box = self._init_extent_box()
         self.bbox = None
 
-        #Start viewer button
+        # Start viewer button
         self.viewer_button = QPushButton("Start iMOD 3D viewer")
         self.viewer_button.clicked.connect(self.start_viewer)
 
@@ -78,7 +79,7 @@ class ImodViewerWidget(QWidget):
         self.fence_buttion = QPushButton("Load fence diagram")
         self.fence_buttion.clicked.connect(self.load_fence_diagram)
 
-        #Define layout
+        # Define layout
         first_column = QVBoxLayout()
         first_column.addWidget(self.layer_selection)
         first_column.addWidget(self.line_picker)
@@ -89,34 +90,33 @@ class ImodViewerWidget(QWidget):
         third_column.addWidget(self.update_button)
         third_column.addWidget(self.fence_buttion)
 
-        layout = QHBoxLayout() #Create horizontal layout, define stretch factors as 1 - 2 - 1
+        layout = (
+            QHBoxLayout()
+        )  # Create horizontal layout, define stretch factors as 1 - 2 - 1
         layout.addLayout(first_column, 1)
         layout.addWidget(self.extent_box, 2)
         layout.addLayout(third_column, 1)
 
         self.setLayout(layout)
 
-        #Initiate dictionary for xml commands
+        # Initiate dictionary for xml commands
         self.xml_dict = {}
 
     def _init_extent_box(self):
         extent_box = QgsExtentGroupBox()
 
         extent_box.setOriginalExtent(
-            self.canvas.extent(),
-            self.canvas.mapSettings().destinationCrs()
+            self.canvas.extent(), self.canvas.mapSettings().destinationCrs()
         )
         extent_box.setCurrentExtent(
-            self.canvas.extent(),
-            self.crs #TODO: Set based on layer crs
+            self.canvas.extent(), self.crs  # TODO: Set based on layer crs
         )
         extent_box.setOutputCrs(self.crs)
 
         return extent_box
 
     def on_close(self):
-        """Ensure rubberbands are removed and picking tools are unset when hiding widget.
-        """
+        """Ensure rubberbands are removed and picking tools are unset when hiding widget."""
 
         self.rectangle_tool.reset()
         self.canvas.unsetMapTool(self.rectangle_tool)
@@ -144,8 +144,7 @@ class ImodViewerWidget(QWidget):
         return uri.split("?")[0].split("file:///")[-1]
 
     def update_data_borehole(self):
-        """Update data for xml command to render boreholes
-        """
+        """Update data for xml command to render boreholes"""
         self.xml_dict = {}
 
         current_layer = self.layer_selection.currentLayer()
@@ -163,26 +162,34 @@ class ImodViewerWidget(QWidget):
         self.xml_dict["column_mapping"] = columnmapping
 
     def update_data_mesh(self):
-        """Update data for xml command to render meshes.
-        """
+        """Update data for xml command to render meshes."""
         self.xml_dict = {}
 
         current_layer = self.layer_selection.currentLayer()
         self.xml_dict["path"] = current_layer.dataProvider().dataSourceUri()
-    
+
         idx = current_layer.datasetGroupsIndexes()
-        self.xml_dict["group_names"] = [current_layer.datasetGroupMetadata(QgsMeshDatasetIndex(group=i)).name() for i in idx]
+        self.xml_dict["group_names"] = [
+            current_layer.datasetGroupMetadata(QgsMeshDatasetIndex(group=i)).name()
+            for i in idx
+        ]
 
-        self.xml_dict["variable_names"] = list(groupby_variable(self.xml_dict["group_names"], idx).keys())
-        self.xml_dict["variable_names"].append("Elevation (cell centre)") #computed by the viewer itsself from 'top' and 'bot'
+        self.xml_dict["variable_names"] = list(
+            groupby_variable(self.xml_dict["group_names"], idx).keys()
+        )
+        self.xml_dict["variable_names"].append(
+            "Elevation (cell centre)"
+        )  # computed by the viewer itsself from 'top' and 'bot'
 
-        style_group_index = idx[0]  #Same style used for all groups in QGIS 3.16
-                                    #FUTURE: Check if this remains
+        style_group_index = idx[0]  # Same style used for all groups in QGIS 3.16
+        # FUTURE: Check if this remains
 
-        colorramp = current_layer.rendererSettings(
-            ).scalarSettings(style_group_index
-            ).colorRampShader(
-            ).colorRampItemList()
+        colorramp = (
+            current_layer.rendererSettings()
+            .scalarSettings(style_group_index)
+            .colorRampShader()
+            .colorRampItemList()
+        )
 
         self.xml_dict["rgb_point_data"] = self.create_rgb_array(colorramp)
 
@@ -190,17 +197,17 @@ class ImodViewerWidget(QWidget):
 
         n_vars = len(self.xml_dict["variable_names"])
 
-        self.xml_dict["guids_grids"] = [uuid.uuid4() for i in range(n_vars+1)]
+        self.xml_dict["guids_grids"] = [uuid.uuid4() for i in range(n_vars + 1)]
 
     def rgb_components_to_float(self, components):
-        return [comp/256 for comp in components]
+        return [comp / 256 for comp in components]
 
     def rgb_string(self, c):
         rgb = c.color.red(), c.color.green(), c.color.blue()
         return "{} {} {} {}".format(c.value, *self.rgb_components_to_float(rgb))
 
     def create_rgb_array(self, colorramp):
-        return ' '.join(self.rgb_string(c) for c in colorramp)
+        return " ".join(self.rgb_string(c) for c in colorramp)
 
     def start_viewer(self):
         self.server.start_server()
@@ -210,41 +217,26 @@ class ImodViewerWidget(QWidget):
         self.update_viewer()
 
     def load_model(self):
-        """Load model from explorer into the renderer
-        """
-        command = xml_tree.command_xml(
-            xml_tree.model_load_tree,
-            **self.xml_dict
-        )
+        """Load model from explorer into the renderer"""
+        command = xml_tree.command_xml(xml_tree.model_load_tree, **self.xml_dict)
         self.server.send(command)
 
     def unload_model(self):
-        """Unload model, removing it from the explorer as well
-        """
-        command = xml_tree.command_xml(
-            xml_tree.model_unload_tree,
-            **self.xml_dict
-        )
+        """Unload model, removing it from the explorer as well"""
+        command = xml_tree.command_xml(xml_tree.model_unload_tree, **self.xml_dict)
         self.server.send(command)
 
     def open_mesh_file(self):
-        """Open file into viewer explorer
-        """
-        command = xml_tree.command_xml(
-            xml_tree.open_file_models_tree, 
-            **self.xml_dict
-            )
+        """Open file into viewer explorer"""
+        command = xml_tree.command_xml(xml_tree.open_file_models_tree, **self.xml_dict)
         self.server.send(command)
 
     def open_borelogs(self):
-        command = xml_tree.command_xml(
-            xml_tree.add_borelogs_tree,
-            **self.xml_dict
-        )
+        command = xml_tree.command_xml(xml_tree.add_borelogs_tree, **self.xml_dict)
         self.server.send(command)
 
     def update_viewer(self):
-        """Update viewer. 
+        """Update viewer.
         First, if created, unload previous model in viewer, also removing it from its explorer.
         Second, update data dictionary.
         Third, open file from a path into viewer explorer.
@@ -255,7 +247,7 @@ class ImodViewerWidget(QWidget):
         if layer is None:
             return
         layer_type = layer.type()
-        
+
         if layer_type == QgsMapLayerType.MeshLayer:
             if "guids_grids" in self.xml_dict.keys():
                 self.unload_model()
@@ -266,7 +258,9 @@ class ImodViewerWidget(QWidget):
             self.update_data_borehole()
             self.open_borelogs()
         else:
-            raise ValueError("File could not be interpreted as borelog ipf or Qgs.MeshLayer")
+            raise ValueError(
+                "File could not be interpreted as borelog ipf or Qgs.MeshLayer"
+            )
 
     def fence_diagram_is_active(self):
         return len(self.line_picker.geometries) > 0
@@ -274,16 +268,16 @@ class ImodViewerWidget(QWidget):
     def prepare_fence_diagram(self):
         self.xml_dict["polylines"] = []
         for linestring in self.line_picker.geometries:
-            xyz_points = [(int(p.x()), int(p.y()), 0) for p in linestring.asPolyline()] #TODO: Integer required??
+            xyz_points = [
+                (int(p.x()), int(p.y()), 0) for p in linestring.asPolyline()
+            ]  # TODO: Integer required??
             self.xml_dict["polylines"].append(itertools.chain(*xyz_points))
 
     def create_fence_diagram(self):
-        """Open file into viewer explorer
-        """
+        """Open file into viewer explorer"""
         command = xml_tree.command_xml(
-            xml_tree.create_fence_diagram_tree, 
-            **self.xml_dict
-            )
+            xml_tree.create_fence_diagram_tree, **self.xml_dict
+        )
         self.server.send(command)
 
     def load_fence_diagram(self):
