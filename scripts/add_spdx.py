3e6da55f3ed1d83f90f17ad5d76e7682b5c18c62
JoerivanEngelen <joerivanengelen@hotmail.com>
2021-06-02 16:51:54 +0200

    Write spdx to all files in imod folder

---
+++ scripts/add_spdx.py
@@ -0,0 +1,24 @@
+import os, sys
+from pathlib import Path
+from glob import glob
+
+SPDX = """\
+# Copyright © 2021 Deltares
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+"""
+
+wdir = (Path(sys.path[0]) / ".." / "imod").resolve()
+
+files = glob(str(wdir / "**" / "*.py"), recursive=True)
+
+with open(files[0], "r") as fd:
+    contents = fd.readlines()
+
+for file in files:
+    with open(file, "r+") as fd:
+        contents = fd.readlines()
+        if contents[0:2] != SPDX.splitlines(True):  # Only add SPDX if not yet in file
+            contents.insert(0, SPDX)  # new_string should end in a newline
+            fd.seek(0)  # readlines consumes the iterator, so we need to start over
+            fd.writelines(contents)  # No need to truncate as we are increasing filesize
