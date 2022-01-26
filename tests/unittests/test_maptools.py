0edb66f78fe861c09b0f956bf9ad40487f845c2f
JoerivanEngelen <joerivanengelen@hotmail.com>
2022-01-26 17:01:57 +0100

    Added tests for line picker widgets

--- tests/unittests/test_maptools.py
+++ tests/unittests/test_maptools.py
@@ -1,11 +1,19 @@
 from qgis.utils import plugins
 from qgis.testing import unittest
-from qgis.core import QgsProject, QgsPointXY
-from qgis.gui import QgsMapCanvas, QgsLayerTreeMapCanvasBridge
+from qgis.core import (
+    QgsProject,
+    QgsPointXY,
+    QgsGeometry,
+    QgsMultiLineString,
+    QgsPoint,
+    QgsLineString,
+)
+from qgis.gui import QgsMapCanvas, QgsLayerTreeMapCanvasBridge, QgsRubberBand
 import sys
 
 from PyQt5.QtCore import Qt, QPoint
 from PyQt5.QtTest import QTest, QSignalSpy
+from PyQt5.QtGui import QColor
 
 
 class TestPickGeometryWidget(unittest.TestCase):
@@ -58,10 +66,266 @@ class TestPickGeometryWidget(unittest.TestCase):
         self.assertTrue(endpoint_is_end == [False, False, True])
 
 
+class TestLineGeometryPickerWidget(unittest.TestCase):
+    def setUp(self):
+        imodplugin = plugins["imodqgis"]
+        # Required call in order to import widgets
+        imodplugin._import_all_submodules()
+
+        from imodqgis.widgets.maptools import LineGeometryPickerWidget, PickGeometryTool
+
+        self.project = QgsProject.instance()
+        self.canvas = QgsMapCanvas()
+        # The bridge makes the link between QgsProject and QgsMapCanvas. So when
+        # a layer is added in the project, it is displayed in the map canvas.
+        # https://gis.stackexchange.com/a/340563
+        bridge = QgsLayerTreeMapCanvasBridge(self.project.layerTreeRoot(), self.canvas)
+
+        self.widget = LineGeometryPickerWidget(self.canvas)
+        self.tooltype = PickGeometryTool
+        self.signalspy = QSignalSpy(self.widget.geometries_changed)
+
+        self.points = [QgsPointXY(200, -199), QgsPointXY(300, -299)]
+        self.expected_geometry = QgsGeometry.fromPolylineXY(self.points)
+
+    def tearDown(self):
+        # Make sure MapTool is unset, otherwise QGIS dies during tests: "QGIS
+        # died on signal 11"
+        self.widget.stop_picking()
+
+    def test_start_picking_map(self):
+        # Test if properly initialized
+        self.assertEqual(self.widget.pick_mode, 0)
+
+        self.widget.start_picking_map()
+
+        self.assertEqual(self.widget.pick_mode, 1)  # == self.widget.PICK_MAP
+        # Test if mapTool set
+        self.assertEqual(type(self.canvas.mapTool()), self.tooltype)
+
+    def test_stop_picking(self):
+        self.widget.stop_picking()
+
+        self.assertEqual(self.widget.pick_mode, 0)
+        self.assertEqual(type(self.canvas.mapTool()), type(None))
+
+    def test_start_and_stop_picking(self):
+        self.widget.start_picking_map()
+
+        self.assertEqual(self.widget.pick_mode, 1)
+        self.assertEqual(type(self.canvas.mapTool()), self.tooltype)
+
+        self.widget.stop_picking()
+
+        self.assertEqual(self.widget.pick_mode, 0)
+        self.assertEqual(type(self.canvas.mapTool()), type(None))
+
+    def test_picker_clicked(self):
+        self.widget.picker_clicked()
+
+        self.assertEqual(self.widget.pick_mode, 1)
+        self.assertEqual(type(self.canvas.mapTool()), self.tooltype)
+
+    def test_picker_clicked_was_active(self):
+        self.widget.start_picking_map()
+        self.widget.picker_clicked()
+
+        self.assertEqual(self.widget.pick_mode, 0)
+        self.assertEqual(type(self.canvas.mapTool()), type(None))
+
+    def test_on_picked_one_point(self):
+        point = [QgsPointXY(200, -199)]
+
+        self.assertEqual(len(self.signalspy), 0)
+
+        self.widget.on_picked(point, False)
+
+        self.assertEqual(self.widget.geometries, [])
+        self.assertEqual(len(self.signalspy), 1)
+
+    def test_on_picked_two_points(self):
+        self.assertEqual(len(self.signalspy), 0)  # No signals emitted
+
+        self.widget.on_picked(self.points, False)
+
+        self.assertGeometriesEqual(self.widget.geometries[0], self.expected_geometry)
+        self.assertEqual(len(self.signalspy), 1)  # Signal now emitted
+
+    def test_on_picked_finished(self):
+        self.assertEqual(len(self.signalspy), 0)
+
+        self.widget.on_picked(self.points, True)
+
+        self.assertGeometriesEqual(self.widget.geometries[0], self.expected_geometry)
+        self.assertEqual(len(self.signalspy), 1)
+        self.assertEqual(self.widget.pick_mode, 0)  # Picking should be stopped
+
+    def test_clear_geometries(self):
+        self.assertEqual(len(self.signalspy), 0)
+
+        self.widget.on_picked(self.points, False)
+        self.widget.clear_geometries()
+
+        self.assertEqual(self.widget.geometries, [])
+        self.assertEqual(len(self.signalspy), 2)
+
+
+class TestMultipleLineGeometryPickerWidget(unittest.TestCase):
+    def setUp(self):
+        imodplugin = plugins["imodqgis"]
+        # Required call in order to import widgets
+        imodplugin._import_all_submodules()
+
+        from imodqgis.widgets.maptools import (
+            MultipleLineGeometryPickerWidget,
+            PickGeometryTool,
+        )
+
+        self.project = QgsProject.instance()
+        self.canvas = QgsMapCanvas()
+        # The bridge makes the link between QgsProject and QgsMapCanvas. So when
+        # a layer is added in the project, it is displayed in the map canvas.
+        # https://gis.stackexchange.com/a/340563
+        bridge = QgsLayerTreeMapCanvasBridge(self.project.layerTreeRoot(), self.canvas)
+
+        self.widget = MultipleLineGeometryPickerWidget(self.canvas)
+        self.tooltype = PickGeometryTool
+
+        self.points = [QgsPointXY(200, -199), QgsPointXY(300, -299)]
+        self.expected_geometry = QgsGeometry.fromPolylineXY(self.points)
+
+        self.points2 = [QgsPointXY(300, -199), QgsPointXY(200, -299)]
+        self.expected_geometry2 = QgsGeometry.fromPolylineXY(self.points2)
+
+    def tearDown(self):
+        # Make sure MapTool is unset, otherwise QGIS dies during tests: "QGIS
+        # died on signal 11"
+        self.widget.stop_picking()
+
+    def test_start_picking_map(self):
+        # Test if properly initialized
+        self.assertEqual(self.widget.pick_mode, 0)
+
+        self.widget.start_picking_map()
+
+        self.assertEqual(self.widget.pick_mode, 1)  # == self.widget.PICK_MAP
+        # Test if mapTool set
+        self.assertEqual(type(self.canvas.mapTool()), self.tooltype)
+
+    def test_stop_picking(self):
+        self.widget.stop_picking()
+
+        self.assertEqual(self.widget.pick_mode, 0)
+        self.assertEqual(type(self.canvas.mapTool()), type(None))
+
+    def test_start_and_stop_picking(self):
+        self.widget.start_picking_map()
+
+        self.assertEqual(self.widget.pick_mode, 1)
+        self.assertEqual(type(self.canvas.mapTool()), self.tooltype)
+
+        self.widget.stop_picking()
+
+        self.assertEqual(self.widget.pick_mode, 0)
+        self.assertEqual(type(self.canvas.mapTool()), type(None))
+
+    def test_on_picked_one_point(self):
+        point = [QgsPointXY(200, -199)]
+
+        self.widget.on_picked(point, False)
+
+        self.assertEqual(self.widget.geometries, [])
+        self.assertEqual(self.widget.last_geometry, None)
+
+        self.assertEqual(self.widget.rubber_bands, None)
+        self.assertEqual(self.widget.last_rubber_band, None)
+
+    def test_on_picked_not_finished(self):
+        self.widget.on_picked(self.points, False)
+
+        self.assertEqual(self.widget.geometries, [])
+        self.assertGeometriesEqual(self.widget.last_geometry, self.expected_geometry)
+
+        self.assertEqual(self.widget.rubber_bands, None)
+        self.assertEqual(type(self.widget.last_rubber_band), QgsRubberBand)
+
+    def test_on_picked_finished(self):
+        self.widget.on_picked(self.points, True)
+        self.widget.on_picked(self.points2, True)
+
+        self.assertEqual(len(self.widget.geometries), 2)
+        self.assertGeometriesEqual(self.widget.geometries[0], self.expected_geometry)
+        self.assertGeometriesEqual(self.widget.geometries[1], self.expected_geometry2)
+        self.assertEqual(self.widget.last_geometry, None)
+
+        self.assertEqual(type(self.widget.rubber_bands), QgsRubberBand)
+        self.assertEqual(self.widget.last_rubber_band, None)
+
+    def test_change_last_geometry(self):
+        self.widget.last_geometry = self.expected_geometry
+        self.widget.change_last_geometry()
+
+        self.assertEqual(type(self.widget.last_rubber_band), QgsRubberBand)
+        self.assertGeometriesEqual(
+            self.widget.last_rubber_band.asGeometry(), self.expected_geometry
+        )
+        self.assertEqual(self.widget.last_rubber_band.width(), 2)
+        self.assertEqual(self.widget.last_rubber_band.fillColor(), QColor(Qt.red))
+
+    def test_draw_geometry_list(self):
+        self.widget.geometries = [self.expected_geometry, self.expected_geometry2]
+        self.widget.draw_geometry_list()
+
+        mls = QgsMultiLineString()
+        for linestring in self.widget.geometries:
+            linestring = [QgsPoint(x=p.x(), y=p.y()) for p in linestring.asPolyline()]
+            linestring = QgsLineString(linestring)
+            mls.addGeometry(linestring)
+
+        expected_geometry = QgsGeometry(mls)
+
+        self.assertEqual(type(self.widget.rubber_bands), QgsRubberBand)
+        self.assertGeometriesEqual(
+            self.widget.rubber_bands.asGeometry(), expected_geometry
+        )
+        self.assertEqual(self.widget.rubber_bands.width(), 2)
+        self.assertEqual(self.widget.rubber_bands.fillColor(), QColor(Qt.red))
+
+    def test_clear_multilines(self):
+        self.widget.geometries = [self.expected_geometry, self.expected_geometry2]
+        self.widget.draw_geometry_list()
+        self.widget.clear_multi_lines()
+
+        self.assertEqual(self.widget.rubber_bands, None)
+        self.assertEqual(self.widget.geometries, [])
+
+    def test_clear_last_line(self):
+        self.widget.last_geometry = self.expected_geometry
+        self.widget.change_last_geometry()
+        self.widget.clear_last_line()
+
+        self.assertEqual(self.widget.last_rubber_band, None)
+        self.assertEqual(self.widget.last_geometry, None)
+
+    def test_clear_rubber_bands(self):
+        self.widget.geometries = [self.expected_geometry, self.expected_geometry2]
+        self.widget.draw_geometry_list()
+        self.widget.last_geometry = self.expected_geometry
+        self.widget.change_last_geometry()
+        self.widget.clear_rubber_bands()
+
+        self.assertEqual(self.widget.rubber_bands, None)
+        self.assertEqual(self.widget.geometries, [])
+        self.assertEqual(self.widget.last_rubber_band, None)
+        self.assertEqual(self.widget.last_geometry, None)
+
+
 def run_all():
     """
     Default function that is called by the runner if nothing else is specified
     """
     suite = unittest.TestSuite()
     suite.addTests(unittest.makeSuite(TestPickGeometryWidget))
+    suite.addTests(unittest.makeSuite(TestLineGeometryPickerWidget))
+    suite.addTests(unittest.makeSuite(TestMultipleLineGeometryPickerWidget))
     unittest.TextTestRunner(verbosity=3, stream=sys.stdout).run(suite)
